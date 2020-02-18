ac979b3fc97617aec4d8238531baa7799cef8a70
RT-KT <austinjs1213@gmail.com>
2020-02-18 17:10:41 -0500

    Update and rename qlh.js to qlh-dyn.js

---
+++ qlh-dyn.js
@@ -0,0 +1,51 @@
+var clickbtn = null;
+var el = document.createElement('el');
+xhook="https://jpillora.com/xhook/dist/xhook.min.js"
+sc=document.createElement("script")
+sc.src=xhook
+document.body.append(sc)
+function HookID() {
+xhook.after(function(request, response) {if(request.url == "/event_logger") {window.re=JSON.parse(request.body);loadDoc("https://quizlet.com/"+getStudyableID())}})
+}
+function getStudyableID() {
+	var i=0;
+	for(i=0;i<re.length;i++) {
+		if(re[i]["event"]["client_studyable_id"] != undefined) {
+			return re[i]["event"]["client_studyable_id"];
+		}
+	}
+}
+function MakeDict() {
+dict={}
+terms=el.getElementsByClassName("TermText notranslate lang-en")
+leng=terms.length-1
+for(i=0; i<leng;i++) {dict[terms[i].innerText]=terms[i+1].innerText;dict[terms[i+1].innerText]=terms[i].innerText}}
+function loadDoc(uri) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      el.innerHTML =
+      this.responseText;
+      MakeDict()
+    }
+  };
+  xhttp.open("GET", uri, true);
+  xhttp.send();
+}
+setTimeout(HookID, 1000)
+function getAns() {
+var prmpt=document.getElementsByClassName("StudentPrompt-inner")[0].innerText
+var answr=dict[prmpt]
+var aTags=document.getElementsByTagName("div")
+for (var i = 0; i < aTags.length; i++) {
+  if (aTags[i].innerHTML == answr) {
+    clickbtn = aTags[i];
+    break;
+  }
+}
+if(clickbtn != null) {
+	clickbtn.click();
+	clickbtn=null;	
+}
+}
+setInterval(getAns, 1500)
